d785d1b817240870e9d5f03cf5713388e0bec55f
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-02-15 17:06:43 -0800

    setItems adds new item to array

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function UserInput() {
 
   const [newItem, setNewItem] = useState('');
+  const [items, setItems] = useState([])
   
 
 
@@ -11,16 +12,15 @@ export default function UserInput() {
   const handleSubmit = (e) => {
     e.preventDefault();
     setNewItem('')
-    
+    setItems([...items, newItem])
   }
 
-  
-
+  console.log(items);
 
   return (
     <form onSubmit={handleSubmit}>
         <input type="text" value={newItem} onChange={(e) => setNewItem(e.target.value)} placeholder="enter new item"/>
-        <button>Add Item</button>
+        <button type="submit">Add Item</button>
         
     </form>
   )

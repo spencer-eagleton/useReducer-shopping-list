a5e63c07902c138460175f3990f6c7cdf2402aa9
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-02-17 18:49:47 -0800

    displayed items array length

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,8 @@ import { useItems } from "../../context/ItemContext"
 
 
 export default function Header() {
-    const { items } = useItems
+    const { items } = useItems();
   return (
-    <div className="header">Header</div>
+    <div className="header">Items on list: {items.length}</div>
   )
 }
